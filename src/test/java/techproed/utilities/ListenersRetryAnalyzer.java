2856ed8ed437d7f4753e82f52688863f5452c733
MustafaDAG33 <mustafadaggg44@gmail.com>
2023-02-06 00:29:38 +0300

    5/02

--- src/test/java/techproed/utilities/ListenersRetryAnalyzer.java
+++ src/test/java/techproed/utilities/ListenersRetryAnalyzer.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Method;
 
 public class ListenersRetryAnalyzer implements IAnnotationTransformer {
     //tum fail test caseleri bir kez daha otomatik olarak run etmek.
+    //bu sinif ListenersRetry sinifini xml de kullanabilmek icin oluturuldu.
     @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
         //bu sinif ListenersRetry util class'a baglidir.
